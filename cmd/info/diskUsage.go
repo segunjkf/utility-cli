fda0530c93de560b42255896f2932ce8af1593c3
segunjkf <sege.timz12@gmail.com>
2023-10-30 15:05:38 +0100

    chore: add viper
    Signed-off-by: segunjkf <sege.timz12@gmail.com>

--- cmd/info/diskUsage.go
+++ cmd/info/diskUsage.go
@@ -1,20 +1,36 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package info
 
 import (
 	"fmt"
 
-	"github.com/spf13/cobra"
 	"github.com/ricochet2200/go-disk-usage/du"
+	"github.com/spf13/cobra"
 )
+
+var (
+	KB = uint64(1024)
+	InputPath string
+
+)
+
+func checkPath(path string)  (string, error) {
+
+	usage := du.NewDiskUsage(path)
+	if usage == nil {
+		return "", fmt.Errorf("the path specified not found: %v", path)
+	}
+
+	return fmt.Sprintf("Used: %d", usage.Used()/(KB*KB)), nil
+	
+}
 
 // diskUsageCmd represents the diskUsage command
 var diskUsageCmd = &cobra.Command{
 	Use:   "diskUsage",
-	Short: "prints diskusage of current directory",
+	Short: "prints disk usage of current directory",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -22,12 +38,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		usage := du.NewDiskUsage(".")
-		fmt.Printf("%v/n", usage)
+
+		if resp, err := checkPath(InputPath); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp)
+		}
 	},
 }
 
 func init() {
+
+	diskUsageCmd.Flags().StringVarP(&InputPath, "path", "p", ".", "path to check Disk usaged")
+
+	if err := diskUsageCmd.MarkFlagRequired("path"); err != nil {
+		fmt.Println(err)
+	}
 	//rootCmd.AddCommand(diskUsageCmd)
 	InfoCmd.AddCommand(diskUsageCmd)
 
--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,21 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
-
 	"github.com/segunjkf/toolbox-cli/cmd/info"
 	"github.com/segunjkf/toolbox-cli/cmd/net"
+
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -35,24 +41,44 @@ func Execute() {
 	}
 }
 
-func addSubCommandPalette(){
-	rootCmd.AddCommand(net.NetCmd)
-	rootCmd.AddCommand(info.InfoCmd)
-}
-
 func init() {
+	cobra.OnInitialize(initConfig)
+
+	// add sub command pallete
+	rootCmd.AddCommand(info.InfoCmd)
+	rootCmd.AddCommand(net.NetCmd)
+
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.toolbox-cli.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.toolbox-cli.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-
-	addSubCommandPalette()
 }
 
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
 
+		// Search config in home directory with name ".toolbox-cli" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".toolbox-cli")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
